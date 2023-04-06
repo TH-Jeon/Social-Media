893ade36ac7f5759e41af5027b1ade974b743b4f
LAPTOP-T21LK98P\KHS <https://github.com/Hosae-Coding>
2023-04-06 13:12:28 -0400

    users route

--- server/index.js
+++ server/index.js
@@ -9,6 +9,7 @@ import morgan from 'morgan';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import authRoutes from './routes/auth.js';
+import userRoutes from './routes/users.js';
 import { register } from './controllers/auth.js';
 
 /* CONFIGURATIONS */
@@ -18,39 +19,40 @@ dotenv.config();
 const app = express();
 app.use(express.json());
 app.use(helmet());
-app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
-app.use(morgan("common"));
-app.use(bodyParser.json({ limit: "30mb", extended: true }));
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(helmet.crossOriginResourcePolicy({ policy: 'cross-origin' }));
+app.use(morgan('common'));
+app.use(bodyParser.json({ limit: '30mb', extended: true }));
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }));
 app.use(cors());
-app.use("/assets", express.static(path.join(__dirname, 'public/assets')));
+app.use('/assets', express.static(path.join(__dirname, 'public/assets')));
 
 /* FILE STORAGE */
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-        cb(null, "public/assets");
-    },
-    filename: function (req, file, cb) {
-        cb(null, file.originalname);
-    }
+   destination: function (req, file, cb) {
+      cb(null, 'public/assets');
+   },
+   filename: function (req, file, cb) {
+      cb(null, file.originalname);
+   },
 });
 
 const upload = multer({ storage });
 
 /* ROUTES WITH FILES */
-app.post("/auth/register", upload.single("picture"), register);
+app.post('/auth/register', upload.single('picture'), register);
 
 /* ROUTES */
-app.use("/auth", authRoutes);
+app.use('/auth', authRoutes);
+app.use('/users', userRoutes);
 
 /* MONGOOSE SETUP */
 const PORT = process.env.PORT || 6001;
 mongoose
-    .connect(process.env.MONGO_URL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-})
-.then(() => {
-    app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
-})
-.catch((error) => console.log(`${error} did not connect`));
+   .connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+   })
+   .then(() => {
+      app.listen(PORT, () => console.log(`Server Port: ${PORT}`));
+   })
+   .catch((error) => console.log(`${error} did not connect`));
